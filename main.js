934a48b994bce3826251d0ca44fe2d36f73ef3bf
akohjesse <akohgodwinjesse@gmail.com>
2021-09-15 13:07:31 +0100

    live js timer

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.scss'
+
+
+
+const currentDate = new Date();
+var w = currentDate.getDay();
+var m = currentDate.getMonth();
+var d = currentDate.getDate()
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+var day = days[w]
+var month = months[m]
+
+document.querySelector('.day').innerHTML= `${day}, ${d} ${month}`;
+
+timer()
+
+async function timer(){
+  var time = await currentDate.toLocaleTimeString('en-US', {
+    hour:"numeric",
+    minute: 'numeric'
+  })
+  document.querySelector('.time').innerHTML = `${time}`;
+  setTimeout(timer, 1000)
+}
